4101b558313304024263a76efd380b8bc2e15c70
owlnical <fred.uggla@owlnical.net>
2019-05-25 22:36:00 +0200

    Generate a pattern with circles

--- main.lua
+++ main.lua
@@ -1,3 +1,30 @@
+function love.load()
+  rng = love.math.newRandomGenerator( )
+  rng:setSeed(os.time())
+  pattern = {
+    order = rng:random(1, 1),
+    amount = rng:random(1, 100),
+    radius = rng:random(1, 100)
+  }
+  width, height = love.graphics.getDimensions()
+  shapes = {}
+  for i=1, pattern.amount, 1 do
+    shapes[#shapes+1] = {
+      x = rng:random(0, width),
+      y = rng:random(0, height),
+      r = rng:random(1, pattern.radius)
+    }
+    print(shapes[#shapes].x, shapes[#shapes].y, shapes[#shapes].r)
+  end
+end
+
 function love.draw()
-    love.graphics.print("Hello World", 400, 300)
+  for k, s in ipairs(shapes) do
+    love.graphics.circle(
+        "fill",
+        s.x,
+        s.y,
+        s.r
+    )
+  end
 end
